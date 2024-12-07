3dc1333f550144f13d8c528759b0c886b9b6f156
Mazed <raiyoz@live.com>
2024-12-07 09:21:36 +0700

    templating section

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from carzone import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
 ]
