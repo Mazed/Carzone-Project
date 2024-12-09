f8def10cb7f2b198d05a3db4b3387bd56b8e70ba
Mazed <raiyoz@live.com>
2024-12-10 05:51:44 +0700

    Completed Car Model

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-from carzone import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
