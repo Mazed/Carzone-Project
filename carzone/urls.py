3d4b6bfa8ead57f125f9b8911674a1d84b64ec28
Mazed <raiyoz@live.com>
2024-12-15 00:18:20 +0700

    Added Cars App and fetced data into homepage

--- carzone/urls.py
+++ carzone/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('cars/', include('cars.urls')),
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
